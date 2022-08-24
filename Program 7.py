9f504e04435cfc2c6ebd6f8152409c3bbdfe88a3
Liam <63742049+TS-7@users.noreply.github.com>
2022-08-24 09:06:57 +0530

    Fix Program 7.py - Use a dictionary not a list

--- Program 7.py
+++ Program 7.py
@@ -2,25 +2,23 @@
 import pickle
 
 def writeb():
-    f= open ("smack.dat","wb+")
-    record = []
+    f= open ("smack.dat","wb")
+    record = {}
+    ch = 'yes'
     while True:
         rno = int(input("Enter roll no: "))
         name = str(input("Enter name: "))
         marks = int(input("Enter marks: "))
-        data = [rno, name, marks]
-        record.append(data)
-        ch= str(input("Would you like to enter more records: "))
-        if ch == 'yes':
-            pass
-        elif ch == 'no':
+        record['Rollno'] = rno
+        record['Name'] = name
+        record['Marks'] = marks
+        pickle.dump(record ,f)
+        ch = input("Would you like to enter more records: ")
+        if ch=='no':
             break
         elif (ch != 'yes' and ch != 'no'):
             print("Invalid input")
             break 
-    pickle.dump(record, f)
     f.close()
-
-writeb()
 
 ''' FRIENDLY REMINDER TO WRITE THE SAMPLE OUTPUT ON THE LEFT SIDE OF THE RECORD '''
